424f2e85bd7f920b7cc93b6aeb53586d7740637d
username <wecp-dummy@gmail.com>
2025-01-02 14:44:19 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/service/impl/CricketerServiceImplJpa.java
@@ -0,0 +1,33 @@
+package com.wecp.progressive.service.impl;
+
+// import java.util.List;
+
+// import com.wecp.progressive.dao.CricketerDAO;
+// import com.wecp.progressive.entity.Cricketer;
+// import com.wecp.progressive.service.CricketerService;
+
+public class CricketerServiceImplJpa {}
+// implements CricketerService  {
+
+//     private CricketerDAO cricketerDAO;
+
+//     public CricketerServiceImplJpa(CricketerDAO cricketerDAO) {
+//         this.cricketerDAO = cricketerDAO;
+//     }
+
+//     @Override
+//     public List<Cricketer> getAllCricketers() {
+//         return List.of();
+//     }
+
+//     @Override
+//     public Integer addCricketer(Cricketer cricketer) {
+//         return -1;
+//     }
+
+//     @Override
+//     public List<Cricketer> getAllCricketersSortedByExperience() {
+//         return List.of();
+//     }
+
+// }
