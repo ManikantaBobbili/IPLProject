215f44fc04236826db58ea6f06f6f92cc45597a8
username <wecp-dummy@gmail.com>
2025-02-06 15:35:39 +0000

    Day-26

--- backend/src/main/java/com/wecp/progressive/config/SecurityConfig.java
+++ backend/src/main/java/com/wecp/progressive/config/SecurityConfig.java
@@ -1,3 +1,4 @@
+
 // package com.wecp.progressive.config;
 
 // import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +29,7 @@
 //     private final PasswordEncoder passwordEncoder;
 
 //     @Autowired
-//     public SecurityConfig(UserDetailsService userDetailsService , JwtRequestFilter jwtRequestFilter , PasswordEncoder passwordEncoder)
-//     {
+//     public SecurityConfig(UserDetailsService userDetailsService, JwtRequestFilter jwtRequestFilter, PasswordEncoder passwordEncoder) {
 //         this.userDetailsService = userDetailsService;
 //         this.jwtRequestFilter = jwtRequestFilter;
 //         this.passwordEncoder = passwordEncoder;
@@ -43,30 +43,9 @@
 //     @Override
 //     protected void configure(HttpSecurity http) throws Exception {
 //         http.cors().and().csrf().disable()
-//                 .authorizeRequests()
-//                 .antMatchers("/user/register" , "/user/login").permitAll()
-//                 .antMatchers(HttpMethod.GET , "/team/**").hasAnyAuthority("USER","ADMIN")
-//                 .antMatchers(HttpMethod.POST , "/team/**").hasAnyAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.PUT , "/team/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.DELETE , "/team/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.GET , "/cricketer/**").hasAnyAuthority("USER","ADMIN")
-//                 .antMatchers(HttpMethod.POST , "/cricketer/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.PUT , "/cricketer/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.DELETE , "/cricketer/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.GET , "/match/**").hasAnyAuthority("USER","ADMIN")
-//                 .antMatchers(HttpMethod.POST , "/match/**").hasAnyAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.PUT , "/match/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.DELETE , "/match/**").hasAuthority("ADMIN")
-//                 .antMatchers(HttpMethod.GET , "/ticket/**").hasAnyAuthority("USER" , "ADMIN")
-//                 .antMatchers(HttpMethod.POST , "/ticket/**").hasAuthority("USER")
-//                 .antMatchers(HttpMethod.DELETE , "/ticket/**").hasAuthority("USER")
-//                 .antMatchers(HttpMethod.GET , "/vote/**").hasAnyAuthority("USER","ADMIN")
-//                 .antMatchers(HttpMethod.POST , "/vote/**").hasAuthority("USER")
-//                 .anyRequest().authenticated()
-//                 .and()
-//                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            
 
-//                 http.addFilterBefore(jwtRequestFilter , UsernamePasswordAuthenticationFilter.class);
+//         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 //     }
 
 //     @Bean
@@ -76,10 +55,9 @@
 //     }
 // }
 
-
-
 package com.wecp.progressive.config;
 
+import com.wecp.progressive.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -95,24 +73,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.wecp.progressive.jwt.JwtRequestFilter;
-
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     private final UserDetailsService userDetailsService;
     private final JwtRequestFilter jwtRequestFilter;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService, JwtRequestFilter jwtRequestFilter, PasswordEncoder passwordEncoder) {
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtRequestFilter jwtRequestFilter,
+                          PasswordEncoder passwordEncoder) {
         this.userDetailsService = userDetailsService;
         this.jwtRequestFilter = jwtRequestFilter;
         this.passwordEncoder = passwordEncoder;
     }
+
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -121,29 +98,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-            .authorizeRequests()
-                .antMatchers("/user/register", "/user/login").permitAll()
-                .antMatchers(HttpMethod.GET, "/team/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/team/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/team/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/team/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.GET, "/cricketer/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/cricketer/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/cricketer/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/cricketer/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.GET, "/match/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/match/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/match/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/match/**").hasAuthority("ADMIN")
-                .antMatchers(HttpMethod.GET, "/ticket/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/ticket/**").hasAuthority("USER")
-                .antMatchers(HttpMethod.DELETE, "/ticket/**").hasAuthority("USER")
-                .antMatchers(HttpMethod.GET, "/vote/**").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/vote/**").hasAuthority("USER")
-                .anyRequest().authenticated()
-            .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable().authorizeRequests()
+                        .antMatchers("/user/register", "/user/login").permitAll()
+                        .antMatchers(HttpMethod.GET, "/team/**").hasAnyAuthority("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/team/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/team/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/team/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/cricketer/**").hasAnyAuthority("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/cricketer/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/cricketer/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/cricketer/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/match/**").hasAnyAuthority("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/match/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/match/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/match/**").hasAuthority("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/ticket/**").hasAnyAuthority("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/ticket/**").hasAuthority("USER")
+                        .antMatchers(HttpMethod.DELETE, "/ticket/**").hasAuthority("USER")
+                        .antMatchers(HttpMethod.GET, "/vote/**").hasAnyAuthority("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/vote/**").hasAuthority("USER")
+                        .anyRequest().authenticated()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
